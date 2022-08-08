6d28523632d757c2d40ef5d18468a9796289f845
Vladimir-Sluchko <vovan1991.s@gmail.com>
2022-08-08 18:43:19 +0300

    mdsj

--- classifier_service/src/main/java/by/itacademy/classifier_service/ClassifierServiceApplication.java
+++ classifier_service/src/main/java/by/itacademy/classifier_service/ClassifierServiceApplication.java
@@ -2,6 +2,7 @@ package by.itacademy.classifier_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
 
